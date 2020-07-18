566185dbbb7520799f69c53b495454922633db4b
MarwanSultan <Marwan.Sultan@gmail.com>
2020-07-18 15:13:58 -0400

    --- Auto-Git Commit ---

---
+++ src/test/java/io/testscripts/HD_Search_Test.java
@@ -0,0 +1,25 @@
+package io.testscripts;
+
+import org.testng.annotations.Test;
+
+import io.pages.HomePage;
+
+public class HD_Search_Test extends BaseTest {
+
+    HomePage homepage;
+
+    @Test
+    public void productSearch() {
+        HomePage homePage = new HomePage(driver);
+        homePage.
+
+
+    }
+
+
+
+}
+
+
+
+
--- src/test/java/io/testscripts/HomeDepot_Test.java
+++ src/test/java/io/testscripts/HomeDepot_Test.java
@@ -3,9 +3,9 @@ package io.testscripts;
 import io.pages.HomePage;
 import org.testng.annotations.Test;
 
-@Test
 public class HomeDepot_Test extends BaseTest {
 
+    @Test
     public void verifyHD_Page() {
         HomePage homePage = new HomePage(driver);
         homePage.landingPage();
